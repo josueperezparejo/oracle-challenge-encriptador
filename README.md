fecc3327ca89455d15ac61a5b231cd50c0a52eaf
josue perez <josue_2707@hotmail.com>
2023-04-26 22:17:51 -0500

    Deploy

---
+++ README.md
@@ -0,0 +1,44 @@
+# Aplicación de encriptación de texto
+
+_Este es un proyecto de una aplicación de encriptación de texto que permite intercambiar mensajes secretos entre personas que conocen la "llave" de encriptación utilizada._
+
+Llaves de encriptación
+
+    La letra "e" es convertida para "enter"
+    La letra "i" es convertida para "imes"
+    La letra "a" es convertida para "ai"
+    La letra "o" es convertida para "ober"
+    La letra "u" es convertida para "ufat"
+
+_Tenga en cuenta que estas "llaves" solo se aplican a letras minúsculas y no se pueden usar letras con acentos ni caracteres especiales._
+
+# Funcionalidades
+
+_La aplicación proporciona las siguientes funcionalidades:_
+
+    Encriptar texto: el usuario puede ingresar un texto y seleccionar la opción "Encriptar" para obtener la versión encriptada del texto.
+    Desencriptar texto: el usuario puede ingresar un texto encriptado y seleccionar la opción "Desencriptar" para obtener la versión original del texto.
+
+# Ejemplos de uso
+
+    Encriptar: "gato" => "gaitober"
+    Desencriptar: "gaitober" => "gato"
+
+# Cómo utilizar la aplicación
+
+_La aplicación es muy fácil de utilizar. Solo siga estos pasos:_
+
+    Ingrese el texto que desea encriptar o desencriptar en el campo correspondiente.
+    Seleccione la opción "Encriptar" o "Desencriptar", según sea necesario.
+    Presione el botón "Correspondiente".
+    El resultado se mostrará en la pantalla.
+
+# Aviso legal
+
+El uso de esta aplicación es bajo su propia responsabilidad. No nos hacemos responsables por el uso indebido de esta aplicación.
+
+---
+
+_Explora más sobre mí en mis perfiles de_ [GitHub](https://github.com/josueperezparejo), [LinkedIn](https://www.linkedin.com/in/josue-david-perez-parejo-769983161), y [Twitch](https://www.twitch.tv/josuepp1997).
+
+Desarrollado por: [Josué Pérez](https://github.com/josueperezparejo) 
---
+++ app.js
@@ -0,0 +1,129 @@
+// Variables & Selectores
+const textArea = document.querySelector('#floatingTextarea');
+const btnEncriptar = document.querySelector('#btnEncriptar');
+const btnDesencriptar = document.querySelector('#btnDesencriptar');
+const inputResultado = document.querySelector('.input-resultado');
+const formulario = document.querySelector('.formulario')
+let resultado
+
+// Eventos
+iniciarApp()
+function iniciarApp() {
+    btnEncriptar.addEventListener('click', () => {
+        resultado = encriptador(textArea.value.toLowerCase())
+        spinner()
+        setTimeout(() => {
+            mostrarAlerta(resultado)
+        }, 2000);
+        textArea.value = ''
+    })
+
+    btnDesencriptar.addEventListener('click', () => {
+        resultado = desencriptador(textArea.value.toLowerCase())
+        spinner()
+        setTimeout(() => {
+            mostrarAlerta(resultado)
+        }, 2000);
+        textArea.value = ''
+    })
+}
+
+// Funciones
+function encriptador(palabra) {
+    palabra = palabra.replace(/e/g, "enter");
+    palabra = palabra.replace(/i/g, "imes");
+    palabra = palabra.replace(/a/g, "ai");
+    palabra = palabra.replace(/o/g, "ober");
+    palabra = palabra.replace(/u/g, "ufat");
+
+    return palabra
+}
+
+function desencriptador(palabra) {
+    palabra = palabra.replace(/enter/g, "e");
+    palabra = palabra.replace(/imes/g, "i");
+    palabra = palabra.replace(/ai/g, "a");
+    palabra = palabra.replace(/ober/g, "o");
+    palabra = palabra.replace(/ufat/g, "u");
+
+    return palabra
+}
+
+function mostrarAlerta(resultado) {
+
+    if (resultado == '') {
+        inputResultado.innerHTML = `
+            <div>
+                <img src="assets/ilustracion.svg" alt="ilustracion">
+                <h2 class="text-center">Ningún mensaje fue encontrado</h2>
+                <p class="text-center">Ingresa el texto que desees encriptar o desencriptar.</p>
+            </div>
+        `
+    } else {
+        inputResultado.innerHTML = `
+            <div class="resultadoBox text-center">
+                <img src="assets/encripted.png" alt="encripted" width="150px">
+                <h2 class="text-center">Resultado 🔐</h2>
+                <p class="border border-success rounded text-center fw-normal inputResult">${resultado}</p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                    <button id="btnCopiar" type="button" class="btn btn-sm btn-outline-success btn-lg px-4">Copiar</button>
+                </div>
+            </div>
+            `
+
+        const btnCopiar = document.querySelector('#btnCopiar');
+
+        btnCopiar.addEventListener('click', () => {
+            const resultado = document.querySelector('.inputResult').textContent
+            const resultadoBox = document.querySelector('.resultadoBox')
+
+            const mensaje = document.createElement('p')
+            mensaje.textContent = 'Copiado Correctamente'
+            mensaje.classList.add('alert', 'alert-success', 'text-center', 'mt-4')
+
+            resultadoBox.appendChild(mensaje)
+
+            setTimeout(() => {
+                mensaje.remove()
+            }, 2000);
+            copiarTexto(resultado)
+        })
+    }
+}
+
+function spinner() {
+    const activo = document.querySelector('.sk-chase')
+    if (!activo) {
+        const spinner = document.createElement('div')
+        spinner.classList.add('sk-chase', 'mx-auto', 'mt-4')
+        spinner.innerHTML = `
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+        `
+
+        const resultadoBox = document.querySelector('.resultadoBox')
+        resultadoBox.innerHTML = `
+            <div class="text-center">
+                <img src="assets/loading.png" alt="loading" width="150px">
+            </div>
+            <p class="text-center fw-bold">Cargando...</p>
+        `
+
+        formulario.appendChild(spinner)
+        setTimeout(() => {
+            spinner.remove()
+        }, 2000);
+    }
+}
+
+async function copiarTexto(texto) {
+    try {
+        await navigator.clipboard.writeText(texto);
+    } catch (error) {
+        console.error('Error al copiar el texto:', error);
+    }
+}
